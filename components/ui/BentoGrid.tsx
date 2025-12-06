df7a3a69939b6627755efcae4adcded40192bb6c
Klowi <antlover.me@gmail.com>
2025-12-06 19:10:44 +0800

    added - on resume

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -165,7 +165,7 @@ export const BentoGridItem = ({
           {id === 6 && (
             <div className="mt-5  lg:mt-0 z-11">
               <div className="flex justify-center items-center cursor-pointer">
-                <a href="/JohnChloeResume.pdf" title="Resume" target="_blank">
+                <a href="/JohnChloe-Resume.pdf" title="Resume" target="_blank">
                   <MagicButton
                     title="Resume"
                     icon={<IoCopy />}
