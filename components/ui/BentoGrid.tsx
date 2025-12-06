d44af649f7a875f5a053fadbcaf53d916025e6c2
Klowi <antlover.me@gmail.com>
2025-12-06 18:09:19 +0800

    before update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -163,7 +163,7 @@ export const BentoGridItem = ({
             </div>
           )}
           {id === 6 && (
-            <div className="mt-5 lg:mt-0 z-11">
+            <div className="mt-5  lg:mt-0 z-11">
               <div className="flex justify-center items-center cursor-pointer">
                 <a href="/JohnChloeResume.pdf" title="Resume" target="_blank">
                   <MagicButton
