1cc5bd40633cfbd325f271b1a82e93f89cd8a7db
Klowi <antlover.me@gmail.com>
2026-01-31 21:02:13 +0800

    changed url for smartagency

--- data/index.ts
+++ data/index.ts
@@ -106,7 +106,7 @@ export const projects = [
       "jsLogo.svg",
     ],
     img: "/smartagency.jpg",
-    link: "https://smartaigency.netlify.app",
+    link: "https://http://smartagency.infinityfree.me/",
     category: ["Web Development"],
     target: "_blank",
     live: true,
